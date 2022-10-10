4458812a479ea25d8da55e4408a2e81b61a20aa1
fjvm09 <fjvm09@gmail.com>
2022-10-09 21:07:30 -0500

    add functionality to operators btns

--- script.js
+++ script.js
@@ -1,4 +1,81 @@
-function add(a, b){return a + b};
-function subtract(a, b){return a - b};
-function multiply(a, b){return a * b};
-function divide(a, b){return b !== 0 ? a / b : 'ERROR'};
+const operatorsBtn = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('.clear');
+const equalBtn = document.querySelector('.equal');
+let valueInUse = 0;
+let storedValue = 0;
+let inputValue = 0;
+let displayValue = 0;
+let operator = '';
+let result = 0;
+
+function add(a, b){result = a + b};
+function subtract(a, b){result = a - b};
+function multiply(a, b){result = a * b};
+function divide(a, b){b !== 0 ? result = a / b : displayValue = 'ERROR'};
+
+function operate(value1, value2){
+    switch(operator){
+        case '+':
+            add(value1, value2)
+            break;
+        case '-':
+            subtract(value1, value2)
+            break;
+        case '*':
+            multiply(value1, value2)
+            break;
+        case '/':
+            divide(value1, value2)
+            break;
+        default:
+            break;
+    }
+}
+
+function changeOperator(e){operator = e.textContent;}
+
+function clear(){
+    valueInUse = 0;
+    displayValue = 0;
+    operator = '';
+    result = 0;
+    storedValue = 0;
+    inputValue = '';
+}
+
+function calculate(){
+    if (inputValue == ''){return}
+    if(operator == ''){
+        storedValue = inputValue;
+        changeOperator(this);
+        inputValue = '';
+        console.log(displayValue)
+    } else {
+        valueInUse = inputValue;
+        operate(storedValue, valueInUse)
+        displayValue = result;
+        storedValue = result;
+        changeOperator(this);
+        inputValue = '';
+        console.log(displayValue)
+    }
+}
+
+function equalsTo(){
+    if (operator == ''){return}
+    if (inputValue == ''){
+        operate(storedValue, valueInUse)
+        displayValue = result;
+        console.log(displayValue)
+        return;
+    }
+    valueInUse = inputValue;
+    operate(storedValue, valueInUse)
+    displayValue = result;
+    inputValue = '';
+    console.log(displayValue)
+}
+
+operatorsBtn.forEach(btn => btn.addEventListener('click', calculate));
+clearBtn.addEventListener('click', clear);
+equalBtn.addEventListener('click', equalsTo)
